4c46d10aed73a2fd75e89f96901298baecf67e78
Anna <ani.kolesnikova@gmail.com>
2018-03-14 09:29:30 +0300

    7/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function count(s, pairs) {
-  // your implementation
+function count(s, pairs) {
+  const prod = pairs.reduce((mul, [p]) => p * mul, 1);
+  const arr = Array(prod).fill('1');
+  pairs.forEach(([p]) => {
+    const d = prod / p;
+    for (let i = 0; i < d; i++) {
+      arr[i * p] = '0';
+    }
+  });
+  const M = pairs.reduce((mul, [p1, p2]) => mul * power(p1, p2 - 1, 1000000007), 1);
+  return (countMasks(arr, s) * M) % 1000000007;
 }
+
+function countMasks(arr, mask) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (checkMask(arr, mask, i)) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+function checkMask(arr, mask, pos) {
+  for (let i = 0; i < mask.length; i++) {
+    if (arr[(i + pos) % arr.length] !== mask[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function power(base, exp, mod) {
+  let pow = 1;
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp === 1) {
+    return base % mod;
+  }
+  if (exp % 2 === 0) {
+    const half = power(base, exp / 2, mod);
+    return (half * half) % mod;
+  }
+  return (base * power(base, exp - 1, mod)) % mod;
+}
+
+module.exports = count;
